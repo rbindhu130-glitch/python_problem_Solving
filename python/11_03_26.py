b4314d666cd5ae4e827514c1e3223b2d9f9fab39
rbindhu1303 <rbindhu130@gmail.com>
2026-03-11 13:36:02 +0530

    Update 11_03_26.py

--- python/11_03_26.py
+++ python/11_03_26.py
@@ -1,1 +1,18 @@
 
+k = 3
+lst = [1,2,3,4,5,6,7,8,9,10]
+
+result = []
+temp = []
+
+for num in lst:
+    temp.append(num)
+
+    if len(temp) == k:
+        result.append(temp)
+        temp = []
+
+if temp:
+    result.append(temp)
+
+print(result)
